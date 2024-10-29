85a1636f396f41328d3b84c244316ff28cb55250
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-10-29 14:09:09 +0100

    [Update]Add more components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,20 @@
 
 
 
-import Header from './components/Header/Headr';
-import AddTaskForm from './components/AddTaskForm/AddTaskForm';
+import React from 'react';
 import './App.css';
-
-
+import { 
+  Header, 
+  TodoApp,
+  Footer,
+} from './components';
 
 function App() {
   return (
     <div className="App">
-          <Header />
-         <AddTaskForm />
-         
-         <div>
-          <ul>
-            <li>
-              <input  type='checkbox'/>
-              <h2>Buld This APP</h2>
-              <button >Delet</button>
-            </li>
-          </ul>
-         </div>
-         <div>
-            2 items
-           </div>
-           <div>
-            <ul>
-              <li><button>All</button></li>
-              <li><button>Active</button></li>
-              <li><button>Completed</button></li>
-
-            </ul>
-           </div>
-    
-
+      <Header />
+      <TodoApp />
+      <Footer />
     </div>
   );
 }
