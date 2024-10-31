e38f696a4f7bd4924825307726e6d7708693338d
nikosajadi <nasibeh.sajjadi@gmail.com>
2024-10-31 13:07:40 +0100

    [Uppdate]Hndle value in form

--- src/components/AddTaskForm/AddTaskForm.jsx
+++ src/components/AddTaskForm/AddTaskForm.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+
+import React, { useState } from "react";
 import "./AddTaskForm.css";
 
 const AddTaskForm = () => {
+    const [value,setValue] = useState('')
+    const handleChange  = (event) => {
+        if(event) event.preventDefault();
+        setValue(event.target.value)
+    } 
+    console.log ("value",value)
+    const handleSubmit =()=>{
+        
+    }
     return(
         <div className="AddTaskForm">
-        <form>
-        <input type='text' placeholder='what need to be done'/>
-        <button type='submit'>Add</button>
+        <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type='text' placeholder='what need to be done'/>
+        <button  type='submit'>Add</button>
         </form>
         </div>
 
